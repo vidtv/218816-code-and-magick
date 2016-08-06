0aa3faa9b1660aef49a2c75a71e8c1dd48166b45
Nikita Borisov <nikit1533@yandex.ru>
2016-08-06 18:25:55 +0300

    Fix of the first task

--- build/js/check.js
+++ build/js/check.js
@@ -1,26 +1,28 @@
 var getMessage = function(a,b) {
-  if (typeof(a) === "boolean") {
+  if (typeof a === 'boolean') {
     if (a === true) {
       return 'Я попал в ' + b;
     }
     else return 'Я никуда не попал';
   }
 
-  else if (typeof(a) === "number") {
+  else if (typeof a === 'number') {
     return 'Я прыгнул на ' + a*100 + ' сантиметров';
   }
 
-  else if (typeof(a) == "object" && typeof(b) != "object") {
+  else if (a instanceof Array && !(b instanceof Array)) {
     var numberOfSteps = 0;
-    for (var i=0; i<a.length; i++)
+    for (var i = 0; i < a.length; i++)
       numberOfSteps = numberOfSteps + a[i];
     return 'Я прошёл ' + numberOfSteps + ' шагов';
   }
 
-  else if (typeof(a) == "object" && typeof(b) == "object") {
+  else if (a instanceof Array && b instanceof Array) {
     var distancePath = 0;
-    for (var i=0; i<a.length; i++)
+    for (var i = 0; i < a.length; i++)
       distancePath = distancePath + a[i]*b[i];
     return 'Я прошёл ' + distancePath + ' метров';
   }
+
+  else return 'Неправильный тип входных параметров!';
 }
