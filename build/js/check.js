549513b0ba61247d17327ec7dd50b782cdbecc88
Nikita Borisov <nikit1533@yandex.ru>
2016-08-06 18:56:13 +0300

    3rd fix of the first task

--- build/js/check.js
+++ build/js/check.js
@@ -3,26 +3,25 @@ var getMessage = function(a,b) {
     if (a) {
       return 'Я попал в ' + b;
     }
-    else return 'Я никуда не попал';
+    return 'Я никуда не попал';
   }
 
-  else if (typeof a === 'number') {
+  if (typeof a === 'number') {
     return 'Я прыгнул на ' + a*100 + ' сантиметров';
   }
 
-  else if (a instanceof Array && !(b instanceof Array)) {
+  if (a instanceof Array && !(b instanceof Array)) {
     var numberOfSteps = 0;
     for (var i = 0; i < a.length; i++)
       numberOfSteps = numberOfSteps + a[i];
     return 'Я прошёл ' + numberOfSteps + ' шагов';
   }
 
-  else if (a instanceof Array && b instanceof Array) {
+  if (a instanceof Array && b instanceof Array) {
     var distancePath = 0;
     for (var i = 0; i < a.length; i++)
       distancePath = distancePath + a[i]*b[i];
     return 'Я прошёл ' + distancePath + ' метров';
   }
-
-  else return 'Неправильный тип входных параметров!';
+    return 'Неправильный тип входных параметров!';
 }
