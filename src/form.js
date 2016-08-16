f9cafa6dd690b4f9dc0d72ee96d69b3bb2fc68d1
Nikita Borisov <nikit1533@yandex.ru>
2016-08-16 16:37:32 +0300

    Troubles was fixed

--- src/form.js
+++ src/form.js
@@ -2,55 +2,62 @@
 
 window.form = (function() {
   var MIN_POSITIVE_RATE = 3;
-  var buttonReview = document.querySelector('button');
   var formContainer = document.querySelector('.overlay-container');
   var formCloseButton = document.querySelector('.review-form-close');
-  var radioButtons = document.getElementsByClassName('review-form-group review-form-group-mark');
-  var reviewAuthor = document.querySelector('#review-name');
-  var reviewDescription = document.querySelector('#review-text');
-  var reviewFields = document.getElementsByClassName('review-form-control review-fields');
-  var reviewRequiredName = document.getElementsByClassName('review-fields-label review-fields-name');
-  var reviewRequiredText = document.getElementsByClassName('review-fields-label review-fields-text');
+  var reviewForm = document.forms[1];
+  var formMarkContainer = reviewForm.querySelector('.review-form-group-mark');
+  var btnReviewSubmit = reviewForm.querySelector('button');
+  var reviewAuthor = reviewForm.elements[7];
+  var reviewText = reviewForm.elements[9];
+  var hintContainer = reviewForm.querySelector('.review-fields');
+  var hintName = hintContainer.querySelector('.review-fields-name');
+  var hintText = hintContainer.querySelector('.review-fields-text');
 
   /*
   функция, устанавливающая полю "Описание" атрибут required при оценке меньше 3
   */
   var reviewTextRequirement = function(event) {
-    reviewDescription.required = Number(event.target.value) < MIN_POSITIVE_RATE;
+    reviewText.required = Number(event.target.value) < MIN_POSITIVE_RATE;
   };
   // и обработчик события для этой функции
-  radioButtons[0].addEventListener('click', reviewTextRequirement);
+  formMarkContainer.addEventListener('click', reviewTextRequirement);
 
   /*
-  обработчик события input, устанавливающий полю "Имя" атрибут required
+   обработчик события input, устанавливающий полю "Имя" атрибут required
   */
   reviewAuthor.addEventListener('input', reviewAuthor.required = true);
 
-  // валидность поля описания и поля имени
-  var isNameValid = Boolean(reviewAuthor.value);
-  var isTextValid = reviewDescription.required && reviewDescription.value || !reviewDescription.required;
+  /*
+  валидность полей имени и описания, а также всей формы
+  */
+  function textValidity(element) {
+    return !element.required || Boolean(element.value.trim());
+  }
 
   /*
   функция, устанавливающая кнопке отправки формы атрибут disabled
   */
   function buttonDisabled() {
-    buttonReview.disabled = !(isNameValid && isTextValid); // кнопка заблочена, если невалиден либо текст, либо имя
+    var isNameValid = textValidity(reviewAuthor);
+    btnReviewSubmit.disabled = !isNameValid; // кнопка заблочена, если невалидно имя
   }
   // и обработчик события для этой функции
   reviewAuthor.addEventListener('input', buttonDisabled);
-  reviewDescription.addEventListener('input', buttonDisabled);
 
 /*
   функция, удаляющая элементы блока review-fields и сам блок
 */
-  function reviewFieldsRemover() {
-    reviewRequiredName[0].classList.toggle('invisible', isNameValid);
-    reviewRequiredText[0].classList.toggle('invisible', isTextValid);
-    reviewFields[0].classList.toggle('invisible', isTextValid && isNameValid);
+  function reviewLabelsRemover() {
+    var isNameValid = textValidity(reviewAuthor);
+    var isTextValid = textValidity(reviewText);
+    var isFormValid = isNameValid && isTextValid;
+    hintName.classList.toggle('invisible', isNameValid);
+    hintText.classList.toggle('invisible', isTextValid);
+    hintContainer.classList.toggle('invisible', isFormValid);
   }
   // и обработчики события для этой функции
-  reviewAuthor.addEventListener('input', reviewFieldsRemover);
-  reviewDescription.addEventListener('input', reviewFieldsRemover);
+  reviewAuthor.addEventListener('input', reviewLabelsRemover);
+  reviewText.addEventListener('input', reviewLabelsRemover);
 
   var form = {
     onClose: null,
