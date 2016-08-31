56ec176788a470ed5c7d576b6ae60f5f7c709462
Nikita Borisov <nikit1533@yandex.ru>
2016-08-31 23:41:15 +0300

    Добавлено подключение и работа с модулем галереи

--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@
 define([
   './form',
   './game',
-  './draw-reviews-list'
-], function(form, Game) {
+  './draw-reviews-list',
+  './gallery'
+], function(form, Game, Gallery) {
   var game = new Game(document.querySelector('.demo'));
   game.initializeLevelAndStart();
   game.setGameStatus(Game.Verdict.INTRO);
@@ -24,4 +25,38 @@ define([
   form.onClose = function() {
     game.setDeactivated(false);
   };
+
+  var photoGallery = document.querySelector('.photogallery');
+
+  // заполняем массив pictures ссылками на изображения в галерее
+  var photos = photoGallery.querySelectorAll('a > img');
+  var pictures = [];
+
+  for(var i = 0; i < photos.length; i++) {
+    pictures.push(photos[i].src);
+  }
+
+  // создаем новый объект галереи
+  var gallery = new Gallery(pictures);
+
+  // коллекция ссылок на фото
+  var linksGallery = photoGallery.querySelectorAll('a');
+
+  // коллекция, преобразованная в массив
+  var linksGalleryArray = Array.prototype.slice.call(linksGallery);
+
+  // обработчик клика по изображению(?) в галерее
+  var galleryOnClick = function(evt) {
+    for (i = 0; i < linksGalleryArray.length; i++) {
+      if(evt.target.src === pictures[i]) {
+        gallery.show(i + 1);
+      }
+    }
+  };
+
+  // применяем этот обработчик при клике на изображение (?)
+  linksGalleryArray.forEach(function(galleryImg) {
+    galleryImg.addEventListener('click', galleryOnClick);
+  });
+
 });
