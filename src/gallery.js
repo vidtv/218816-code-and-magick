59a68d34b90405c98120692f9d1e056d0a023c17
Nikita Borisov <nikit1533@yandex.ru>
2016-09-04 13:53:17 +0300

    Версия модуля галереи с обработчиком клика на блоке .photogallery

--- src/gallery.js
+++ src/gallery.js
@@ -4,7 +4,8 @@ define(function() {
 
   function Gallery(pictures) {
     var self = this;
-    this.activePicture = 0;
+    this.pictures = pictures;
+    this.activePicture = 1;
     this.galleryElement = document.querySelector('.overlay-gallery');
     this.galleryToggleLeft = document.querySelector('.overlay-gallery-control-left');
     this.galleryToggleRight = document.querySelector('.overlay-gallery-control-right');
@@ -15,48 +16,64 @@ define(function() {
     this.galleryPicturesCount.innerHTML = pictures.length;
 
     this.galleryClose.onclick = function() {
-      self.onCloseClick();
+      self.hide();
     };
   }
 
   var photoGallery = document.querySelector('.photogallery');
 
   // функция для появления блока фотогалереи
-  var makeGalleryVisible = function() {
+  Gallery.prototype.makeGalleryVisible = function() {
     this.galleryElement.classList.remove('invisible');
   };
 
   // пролистывание фотогалереи влево
-  var listGalleryLeft = function() {
-    this.setActivePicture(this.activePicture - 1);
+  Gallery.prototype.listGalleryLeft = function() {
+    if (this.activePicture > 1) {
+      this.setActivePicture(this.activePicture - 1);
+    }
   };
 
   // пролистывание фотогалереи вправо
-  var listGalleryRight = function() {
-    this.setActivePicture(this.activePicture + 1);
+  Gallery.prototype.listGalleryRight = function() {
+    if(this.activePicture < 6) {
+      this.setActivePicture(this.activePicture + 1);
+    }
   };
 
   // показ фотогалереи
   Gallery.prototype.show = function(pictureNumber) {
-    photoGallery.addEventListener('click', makeGalleryVisible);
-    this.galleryToggleLeft.addEventListener('click', listGalleryLeft);
-    this.galleryToggleRight.addEventListener('click', listGalleryRight);
+    var self = this;
+
+    photoGallery.onclick = function() {
+      self.makeGalleryVisible();
+    };
+
+    this.galleryToggleLeft.onclick = function() {
+      self.listGalleryLeft();
+    };
+
+    this.galleryToggleRight.onclick = function() {
+      self.listGalleryRight();
+    };
+
     this.setActivePicture(pictureNumber);
   };
 
   // спрятать фотогалерею
   Gallery.prototype.hide = function() {
     this.galleryElement.classList.add('invisible');
-    this.galleryToggleLeft.removeEventListener('click', listGalleryLeft);
-    this.galleryToggleRight.removeEventListener('click', listGalleryRight);
-    this.galleryClose.onclick = null;
-    photoGallery.removeEventListener('click', makeGalleryVisible);
+    this.galleryToggleLeft.removeEventListener('click', this.listGalleryLeft);
+    this.galleryToggleRight.removeEventListener('click', this.listGalleryRight);
+    this.photoGallery.removeEventListener('click', this.makeGalleryVisible);
   };
 
+  // установка текущей просматриваемой фотографии
   Gallery.prototype.setActivePicture = function(pictureNumber) {
     this.activePicture = pictureNumber;
     var currentPicture = new Image();
     currentPicture.src = this.pictures[pictureNumber - 1];
+
     var galleryPicturePreview = document.querySelector('.overlay-gallery-preview');
     var oldPicture = galleryPicturePreview.querySelector('img');
     if (oldPicture) {
