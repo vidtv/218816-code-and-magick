e565792eb644f143a71ff12a926dcb5c78474670
Nikita Borisov <nikit1533@yandex.ru>
2016-09-12 01:41:49 +0300

    ДОбавление и удаление классов сведены в одну функцию

--- src/review.js
+++ src/review.js
@@ -2,35 +2,31 @@
 
 define(function() {
   var reviewTemplate = document.querySelector('template');
-  var elementToClone = (reviewTemplate || reviewTemplate.content).querySelector('.review');
+  //var elementToClone = (reviewTemplate || reviewTemplate.content).querySelector('.review');
+  var elementToClone;
+  if('content' in reviewTemplate) {
+    elementToClone = reviewTemplate.content.querySelector('.review');
+  } else {
+    elementToClone = reviewTemplate.querySelector('.review');
+  }
+
+  var reviewQuiz = document.querySelector('.review-quiz');
 
   function Review(data) {
     this.data = data;
     this.element = elementToClone.cloneNode(true);
+    reviewQuiz.addEventListener('click', this.makeReviewAnswerActive);
+    reviewQuiz.removeEventListener('click', this.makeReviewAnswerActive);
   }
 
-  var reviewQuiz = document.querySelectorAll('.review-quiz-answer');
-
-  Review.prototype.addReviewAnswerActive = function() {
-    reviewQuiz.classList.add('.review-quiz-answer-active');
-  };
-
-  Review.prototype.makeReviewAnswerActive = function() {
-    var self = this;
-    reviewQuiz.onclick = function() {
-      self.addReviewAnswerActive();
-    };
-  };
-
-  Review.prototype.removeReviewAnswerActive = function() {
-    reviewQuiz.classList.remove('.review-quiz-answer-active');
-  };
-
-  Review.prototype.makeReviewAnswerActive = function() {
-    var self = this;
-    reviewQuiz.onclick = function() {
-      self.removeReviewAnswerActive();
-    };
+  Review.prototype.makeReviewAnswerActive = function(event) {
+    var AnswerActive = reviewQuiz.querySelector('.review-quiz-answer-active');
+    if(event.target.classList.contains('.review-quiz-answer')) {
+      if(AnswerActive) {
+        AnswerActive.classList.remove('.review-quiz-answer-active');
+      }
+      event.target.classList.add('.review-quiz-answer-active');
+    }
   };
 
   return Review;
