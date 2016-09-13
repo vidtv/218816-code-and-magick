b1e92053065562c9b8716f419e497142dd3592d4
Nikita Borisov <nikit1533@yandex.ru>
2016-09-13 21:27:54 +0300

    Отрисовка изображений в отзыве перенесена в модуль review.js

--- src/make-review.js
+++ src/make-review.js
@@ -5,43 +5,14 @@ define(['./review'], function(Review) {
     // прячем блок с фильтром отзывов
     var reviewFilter = document.querySelector('.reviews-filter');
     reviewFilter.classList.add('invisible');
-      // добавляем контейнер с отзывами
-    var reviewsContainer = document.querySelector('.reviews-list');
 
     var newReview = new Review(data);
     var reviewMark = newReview.element.querySelector('.review-rating');
     var reviewDescription = newReview.element.querySelector('.review-text');
-    var reviewAuthor = newReview.element.querySelector('.review-author');
-    var IMAGE_SIZE = 124;
-    var IMAGE_LOAD_TIMER = 4000;
     var RATING_STAR_WIDTH = 40;
-
     reviewDescription.textContent = data.description;
     reviewMark.style.width = RATING_STAR_WIDTH * data.rating + 'px';
 
-    var reviewAuthorImage = new Image(IMAGE_SIZE, IMAGE_SIZE);
-    newReview.element.replaceChild(reviewAuthorImage, reviewAuthor);
-    reviewAuthorImage.classList.add('review-author');
-    var reviewImageLoadTimeout;
-
-    reviewAuthorImage.onload = function() {
-      clearTimeout(reviewImageLoadTimeout);
-      reviewAuthorImage.src = data.author.picture;
-      reviewAuthorImage.title = data.author.name;
-    };
-
-    reviewAuthorImage.onerror = function() {
-      newReview.element.classList.add('review-load-failure');
-    };
-
-    reviewAuthorImage.src = data.author.picture;
-
-    reviewImageLoadTimeout = setTimeout(function() {
-      reviewAuthorImage.removeAttribute('src');
-      newReview.element.classList.add('review-load-failure');
-    }, IMAGE_LOAD_TIMER);
-
-    reviewsContainer.appendChild(newReview.element);
     reviewFilter.classList.remove('invisible');
   };
 });
--- src/review.js
+++ src/review.js
@@ -10,24 +10,57 @@ define(function() {
     elementToClone = reviewTemplate.querySelector('.review');
   }
 
-  var reviewQuiz = document.querySelector('.review-quiz');
-
   function Review(data) {
     this.data = data;
     this.element = elementToClone.cloneNode(true);
-    reviewQuiz.addEventListener('click', this.makeReviewAnswerActive);
-    reviewQuiz.removeEventListener('click', this.makeReviewAnswerActive);
+    this.Yes = this.element.querySelector('.review-quiz-answer-yes');
+    this.No = this.element.querySelector('.review-quiz-answer-no');
+
+    this.Yes.addEventListener('click', this.makeReviewAnswerActive);
+    this.No.addEventListener('click', this.makeReviewAnswerActive);
+    //this.reviewQuiz = this.element.querySelector('.review-quiz');
   }
 
   Review.prototype.makeReviewAnswerActive = function(event) {
-    var AnswerActive = reviewQuiz.querySelector('.review-quiz-answer-active');
-    if(event.target.classList.contains('.review-quiz-answer')) {
+    var AnswerActive = this.element.querySelector('.review-quiz-answer-active');
+    if(event.target.classList.contains('review-quiz-answer')) {
       if(AnswerActive) {
-        AnswerActive.classList.remove('.review-quiz-answer-active');
+        AnswerActive.classList.remove('review-quiz-answer-active');
       }
-      event.target.classList.add('.review-quiz-answer-active');
+      event.target.classList.add('review-quiz-answer-active');
     }
   };
+
+  var IMAGE_SIZE = 124;
+  var IMAGE_LOAD_TIMER = 4000;
+
+  // добавляем контейнер с отзывами
+  var reviewsContainer = document.querySelector('.reviews-list');
+
+  var reviewAuthor = this.element.querySelector('.review-author');
+  var reviewAuthorImage = new Image(IMAGE_SIZE, IMAGE_SIZE);
+  this.element.replaceChild(reviewAuthorImage, reviewAuthor);
+  reviewAuthorImage.classList.add('review-author');
+  var reviewImageLoadTimeout;
+
+  reviewAuthorImage.onload = function() {
+    clearTimeout(reviewImageLoadTimeout);
+    reviewAuthorImage.src = this.data.author.picture;
+    reviewAuthorImage.title = this.data.author.name;
+  };
+
+  reviewAuthorImage.onerror = function() {
+    this.element.classList.add('review-load-failure');
+  };
+
+  reviewAuthorImage.src = this.data.author.picture;
+
+  reviewImageLoadTimeout = setTimeout(function() {
+    reviewAuthorImage.removeAttribute('src');
+    this.element.classList.add('review-load-failure');
+  }, IMAGE_LOAD_TIMER);
+
+  reviewsContainer.appendChild(this.element);
 
   return Review;
 });
