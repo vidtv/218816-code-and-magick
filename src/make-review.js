7ae12a5c5fb1e6467f0298fdcf578990e2eb73f2
Nikita Borisov <nikit1533@yandex.ru>
2016-09-15 11:04:15 +0300

    Второй класс (#13)
    * Добавлен и подключен модуль Review
    
    * Переписаны методы класса Review
    
    * ДОбавление и удаление классов сведены в одну функцию
    
    * Отрисовка изображений в отзыве перенесена в модуль review.js
    
    * Исправлена отрисовка отзывов
    
    * Добавлен короткий способ задания elementToClone
    
    * Доработано отключение класса active при повторном клике на review-quiz
    
    * Метод отрисовки картинки отзыва разбит на две функции

--- src/make-review.js
+++ src/make-review.js
@@ -1,55 +1,15 @@
 'use strict';
 
-define(function() {
+define(['./review'], function(Review) {
   return function(data) {
     // прячем блок с фильтром отзывов
     var reviewFilter = document.querySelector('.reviews-filter');
     reviewFilter.classList.add('invisible');
-      // добавляем контейнер с отзывами
-    var reviewsContainer = document.querySelector('.reviews-list');
-    var reviewTemplate = document.querySelector('template');
-    var elementToClone;
 
-    if('content' in reviewTemplate) {
-      elementToClone = reviewTemplate.content.querySelector('.review');
-    } else {
-      elementToClone = reviewTemplate.querySelector('.review');
-    }
+    var newReview = new Review(data);
 
-    var reviewElement = elementToClone.cloneNode(true);
-    var reviewMark = reviewElement.querySelector('.review-rating');
-    var reviewDescription = reviewElement.querySelector('.review-text');
-    var reviewAuthor = reviewElement.querySelector('.review-author');
-    var IMAGE_SIZE = 124;
-    var IMAGE_LOAD_TIMER = 4000;
-    var RATING_STAR_WIDTH = 40;
+    newReview.addNewReview();
 
-    reviewDescription.textContent = data.description;
-    reviewMark.style.width = RATING_STAR_WIDTH * data.rating + 'px';
-
-    var reviewAuthorImage = new Image(IMAGE_SIZE, IMAGE_SIZE);
-    reviewElement.replaceChild(reviewAuthorImage, reviewAuthor);
-    reviewAuthorImage.classList.add('review-author');
-    var reviewImageLoadTimeout;
-
-    reviewAuthorImage.onload = function() {
-      clearTimeout(reviewImageLoadTimeout);
-      reviewAuthorImage.src = data.author.picture;
-      reviewAuthorImage.title = data.author.name;
-    };
-
-    reviewAuthorImage.onerror = function() {
-      reviewElement.classList.add('review-load-failure');
-    };
-
-    reviewAuthorImage.src = data.author.picture;
-
-    reviewImageLoadTimeout = setTimeout(function() {
-      reviewAuthorImage.removeAttribute('src');
-      reviewElement.classList.add('review-load-failure');
-    }, IMAGE_LOAD_TIMER);
-
-    reviewsContainer.appendChild(reviewElement);
     reviewFilter.classList.remove('invisible');
   };
 });
---
+++ src/review.js
@@ -0,0 +1,73 @@
+'use strict';
+
+define(function() {
+
+  var RATING_STAR_WIDTH = 40;
+  var IMAGE_SIZE = 124;
+  var reviewTemplate = document.querySelector('template');
+  var elementToClone = (reviewTemplate.content || reviewTemplate).querySelector('.review');
+
+  function Review(data) {
+    this.data = data;
+    this.element = elementToClone.cloneNode(true);
+    this.yes = this.element.querySelector('.review-quiz-answer-yes');
+    this.no = this.element.querySelector('.review-quiz-answer-no');
+    this.reviewAuthor = this.element.querySelector('.review-author');
+    this.reviewMark = this.element.querySelector('.review-rating');
+    this.reviewDescription = this.element.querySelector('.review-text');
+
+    this.reviewImage = new Image(IMAGE_SIZE, IMAGE_SIZE);
+
+    this.yes.addEventListener('click', this.makeReviewAnswerActive.bind(this));
+    this.no.addEventListener('click', this.makeReviewAnswerActive.bind(this));
+  }
+
+  Review.prototype.makeReviewAnswerActive = function(event) {
+    var answerActive = this.element.querySelector('.review-quiz-answer-active');
+    if(answerActive) {
+      answerActive.classList.remove('review-quiz-answer-active');
+    }
+    if(event.target !== answerActive) {
+      event.target.classList.add('review-quiz-answer-active');
+    }
+  };
+
+  // добавляем отзывы
+  Review.prototype.addNewReview = function() {
+    this.reviewsContainer = document.querySelector('.reviews-list');
+
+    this.reviewDescription.textContent = this.data.description;
+    this.reviewMark.style.width = RATING_STAR_WIDTH * this.data.rating + 'px';
+    var self = this;
+
+    self.addClassesToReviewImage();
+
+    this.reviewsContainer.appendChild(this.element);
+  };
+
+  Review.prototype.addClassesToReviewImage = function() {
+    function LoadOrNot(notFail) {
+      if(notFail) {
+        this.reviewImage.title = this.data.author.name;
+      } else {
+        this.reviewImage.src = '';
+        this.element.classList.add('review-load-failure');
+      }
+    }
+    this.funcCheckLoad(this.data.author.picture, LoadOrNot.bind(this));
+  };
+
+  Review.prototype.funcCheckLoad = function(src, callBack) {
+    this.element.replaceChild(this.reviewImage, this.reviewAuthor);
+    this.reviewImage.classList.add('review-author');
+    this.reviewImage.onload = function() {
+      callBack(true);
+    };
+    this.reviewImage.onerror = function() {
+      callBack(false);
+    };
+    this.reviewImage.src = src;
+  };
+
+  return Review;
+});
