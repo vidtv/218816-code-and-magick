7c4d51d463b2864a50c6849d01d5efc46afeb5c5
Nikita Borisov <nikit1533@yandex.ru>
2016-09-17 21:12:32 +0300

    Работающая отрисовка отзывов

--- src/draw-reviews-list.js
+++ src/draw-reviews-list.js
@@ -7,15 +7,22 @@ define([
   './make-review'
 ], function(load, renderReview) {
   var REVIEW_COUNT = 3;
+  var currentReviewNumber = 0;
   var moreReviewsButton = document.querySelector('.reviews-controls-more');
   var loadReviews = function(currentReview) {
     load(HTTP_REQUEST_URL, {
       from: currentReview,
-      to: currentReview + REVIEW_COUNT
-    }, renderReview);
+      to: currentReview + REVIEW_COUNT,
+      filter: 'reviews-all'
+    }, function(data) {
+      data.forEach(renderReview);
+    });
+    moreReviewsButton.classList.remove('invisible');
   };
+
+  loadReviews(currentReviewNumber);
+
   moreReviewsButton.addEventListener('click', function() {
-    var currentReviewNumber = 0;
     loadReviews(currentReviewNumber += REVIEW_COUNT);
   });
 });
